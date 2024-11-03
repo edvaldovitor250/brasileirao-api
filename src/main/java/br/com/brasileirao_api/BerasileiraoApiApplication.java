682c29f658f42b3b68cd4f46a3775bda8ab71b16
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-11-03 17:52:55 -0300

    feat: start project first version

---
+++ src/main/java/br/com/brasileirao_api/BerasileiraoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.brasileirao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerasileiraoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerasileiraoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brasileirao_api/BerasileiraoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brasileirao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerasileiraoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
