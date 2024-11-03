67b812e5e3c302f44bd3e6d54d5ed7341d6bb62c
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-11-03 19:41:28 -0300

    feat: create DTOs in PartidaGoogleDTO

---
+++ src/main/java/br/com/brasileirao_api/dto/PartidaGoogleDTO.java
@@ -0,0 +1,28 @@
+package br.com.brasileirao_api.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Data
+public class PartidaGoogleDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String statusPartida;
+    private String tempoPartida;
+
+    private String nomeEquipeCasa;
+    private String urlLogoEquipeCasa;
+    private Integer placarEquipeCasa;
+    private String golsEquipeCasa;
+    private String placarEstendidoEquipeCasa;
+
+    private String nomeEquipeVisitante;
+    private String urlLogoEquipeVisitante;
+    private Integer placarEquipeVisitante;
+    private String golsEquipeVisitante;
+    private String placarEstendidoEquipeVisitante;
+
+
+}
