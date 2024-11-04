b852088b606f4b44690c5792075eb7bb88de5321
EdvaldoVitor <edvaldovitor250@gmail.comn>
2024-11-04 09:41:38 -0300

    test for commit

--- src/main/java/br/com/brasileirao_api/dto/PartidaGoogleDTO.java
+++ src/main/java/br/com/brasileirao_api/dto/PartidaGoogleDTO.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 @Data
 public class PartidaGoogleDTO implements Serializable {
 
+
     private static final long serialVersionUID = 1L;
 
     private String statusPartida;
