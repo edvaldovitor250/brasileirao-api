04f3b114df5bf14f6da0ba1230b44b49d8541557
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-11-10 09:16:42 -0300

    feat: created in EquipeController fuction for Get

--- src/main/java/br/com/brasileirao_api/controller/EquipeController.java
+++ src/main/java/br/com/brasileirao_api/controller/EquipeController.java
@@ -1,12 +1,54 @@
 package br.com.brasileirao_api.controller;
 
-import io.swagger.annotations.Api;
+import br.com.brasileirao_api.dto.equipe.EquipeResponseDTO;
+import br.com.brasileirao_api.exception.StandardError;
+import br.com.brasileirao_api.model.Equipe;
+import br.com.brasileirao_api.service.EquipeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api("API de equipes")
 @RestController
 @RequestMapping("/api/v1/equipes")
-public class EquipeController{
+@AllArgsConstructor
+public class EquipeController {
 
+    private final EquipeService equipeService;
+
+    @Operation(summary = "Busca uma equipe pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = {@Content(schema = @Schema(implementation = Equipe.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = {@Content(schema = @Schema(implementation = StandardError.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = {@Content(schema = @Schema(implementation = StandardError.class))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = {@Content(schema = @Schema(implementation = StandardError.class))}),
+            @ApiResponse(responseCode = "404", description = "Not found", content = {@Content(schema = @Schema(implementation = StandardError.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = {@Content(schema = @Schema(implementation = StandardError.class))})
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<Equipe> buscarEquipeId(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(equipeService.buscarEquipeId(id));
+    }
+
+
+    @Operation(summary = "Listar equipe")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = {@Content(schema = @Schema(implementation = Equipe.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = {@Content(schema = @Schema(implementation = StandardError.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = {@Content(schema = @Schema(implementation = StandardError.class))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = {@Content(schema = @Schema(implementation = StandardError.class))}),
+            @ApiResponse(responseCode = "404", description = "Not found", content = {@Content(schema = @Schema(implementation = StandardError.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = {@Content(schema = @Schema(implementation = StandardError.class))})
+    })
+    @GetMapping("")
+    public ResponseEntity<EquipeResponseDTO> listarEquipes(){
+        return  ResponseEntity.ok().body(equipeService.listarEquipes());
+    }
 }
--- src/main/java/br/com/brasileirao_api/service/EquipeService.java
+++ src/main/java/br/com/brasileirao_api/service/EquipeService.java
@@ -1,7 +1,27 @@
 package br.com.brasileirao_api.service;
 
+import br.com.brasileirao_api.dto.equipe.EquipeResponseDTO;
+import br.com.brasileirao_api.exception.NotFoundException;
+import br.com.brasileirao_api.model.Equipe;
+import br.com.brasileirao_api.repository.EquipeRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class EquipeService {
+
+    private final EquipeRepository equipeRepository;
+
+
+    public Equipe buscarEquipeId(Long id) {
+        return equipeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Nenhuma equipe encontrada com o id informado: " + id));
+    }
+
+    public EquipeResponseDTO listarEquipes() {
+        EquipeResponseDTO equipes = new EquipeResponseDTO();
+        equipes.setEquipes(equipeRepository.findAll());
+        return equipes;
+    }
 }
